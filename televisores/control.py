dd2c2307f84d8223d20da07d2caf7127e01c3a93
sidiazr <sidiazr@unal.edu.co>
2024-11-17 18:35:03 -0500

    Intento 1

---
+++ televisores/control.py
@@ -0,0 +1,37 @@
+class Control:
+    def __init__(self):
+        self._tv = None
+    
+    def turnOn(self):
+        self._tv.turnOn()
+    
+    def turnOff(self):
+        self._tv.turnOff()
+
+    def canalUp(self):
+        self._tv.canalUp()
+
+    def canalDown(self):
+        self._tv.canalDown
+
+    def volumenUp(self):
+        self._tv.volumenUp()
+
+    def volumenDown(self):
+        self._tv.volumenDown()
+
+    def setCanal(self, num):
+        self._tv.setCanal(num)
+
+    def setVolumen(self, num):
+        self._tv.setVolumen(num)
+
+    def enlazar(self, tv):
+        self._tv = tv
+        tv.setControl(self)
+    
+    def getTv(self):
+        return self._tv
+    
+    def setTv(self, tv):
+        self._tv = tv
